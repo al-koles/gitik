07e0e768bb3820c92fe6d053be1b5793e81d0bf9
Oleksii Kolesnyk <olexii.kolesnyk.w@gmail.com>
2024-04-25 21:46:02 +0300

    Add git pull command executing

--- Gitik.Console/Program.cs
+++ Gitik.Console/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+var repos = Directory.GetDirectories("./", "*", SearchOption.TopDirectoryOnly);
+
+foreach (var repoPath in repos)
+{
+    // Console.WriteLine("Pulling" + repoPath);
+    Console.WriteLine(await Pull(repoPath));
+    Console.WriteLine("---");
+}
+
+static async Task<string> Pull(string repoPath)
+{
+    var command = $"C/ cd {repoPath} && git pull";
+    
+    var process = new Process();
+    var startInfo = new ProcessStartInfo
+    {
+        RedirectStandardOutput = true,
+        // UseShellExecute = false,
+        // CreateNoWindow = true,
+        FileName = "git",
+        Arguments = "pull",
+        WorkingDirectory = repoPath,
+    };
+    process.StartInfo = startInfo;
+    process.Start();
+
+    var gitPullResponse = await process.StandardOutput.ReadToEndAsync();
+    
+    return $"{repoPath} git pull{Environment.NewLine}{gitPullResponse}";
+}
