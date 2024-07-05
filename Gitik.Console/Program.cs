eb980fd505b01405aada39f2d806eba20d1c31ad
Oleksii Kolesnyk <oleksii.kolesnyk.w@gmail.com>
2024-07-05 18:48:37 +0300

    Add command line command

--- Gitik.Console/Program.cs
+++ Gitik.Console/Program.cs
@@ -1,115 +1,143 @@
-﻿using System.Diagnostics;
+﻿using System.CommandLine;
+using System.Diagnostics;
 
-if (args.Any(a => a == "--debug"))
+var rootCommand = new RootCommand("Helper for git.");
+
+var debugOption = new Option<bool>(
+    name: "--debug",
+    description: "Attach debugger to the process"
+);
+var maxPullProcessesOption = new Option<int?>(
+    ["-m", "--max"],
+    () => 10,
+    "Max pull processes");
+
+var pullCommand = new Command("pull", "Execute git pull for repositories in current directory")
 {
-    Console.WriteLine("Attaching debugger");
-    Debugger.Launch();
-}
-
-var currentDir = Directory.GetCurrentDirectory();
-Console.WriteLine("Searchinng repos in " + currentDir);
-
-var repos = Directory.GetDirectories("./", "*", SearchOption.TopDirectoryOnly)
-    .Where(r => Directory.GetDirectories(r, ".git", SearchOption.TopDirectoryOnly).Any())
-    .ToArray();
-if (!repos.Any())
+    debugOption,
+    maxPullProcessesOption,
+};
+pullCommand.SetHandler((debug, maxPullProcesses) =>
 {
-    Console.WriteLine("No repositories found");
-    return;
-}
+    Console.WriteLine($"{nameof(debug)}: {debug}");
+    Console.WriteLine($"{nameof(maxPullProcesses)}: {maxPullProcesses}");
+}, debugOption, maxPullProcessesOption);
+rootCommand.AddCommand(pullCommand);
 
-Console.WriteLine("Detected repos:" + Environment.NewLine + string.Join(Environment.NewLine, repos));
-Console.WriteLine();
-Console.WriteLine("---PULLING STARTED---");
-Console.WriteLine();
+await rootCommand.InvokeAsync(args);
 
-var processes = repos.Select(r => PullWithHandling(r)).ToList();
-var responses = new List<PullResponse>();
-while (processes.Any())
-{
-    var finished = await Task.WhenAny(processes);
-    processes.Remove(finished);
-    var response = await finished;
-    responses.Add(response);
-
-    Console.WriteLine(response.Response);
-    Console.WriteLine("---");
-    Console.WriteLine();
-}
-
-Console.WriteLine("---PULLING FINISHED---");
-
-var havingError = responses.Where(r => !r.IsSuccess).Select(r => r.Repo).ToList();
-var havingChanges = responses
-    .Where(r => !r.Response.Contains("Already up to date.") &&
-                !havingError.Contains(r.Repo))
-    .Select(r => r.Repo)
-    .ToList();
-
-PrintAsList(havingChanges, "Changes applied");
-PrintAsList(havingError, "Responded error");
-
-static void PrintAsList(List<string> items, string name)
-{
-    Console.WriteLine();
-    Console.WriteLine($"---{name}---");
-    Console.WriteLine(items.Any() ? string.Join(Environment.NewLine, items) : "no data");
-    Console.WriteLine("---------------------");
-}
-
-static async Task<PullResponse> PullWithHandling(string repoPath)
-{
-    try
-    {
-        return await Pull(repoPath);
-    }
-    catch (Exception e)
-    {
-        return new PullResponse(false, repoPath, $"Error pulling '{repoPath}'" + e.Message);
-    }
-}
-
-static async Task<PullResponse> Pull(string repoPath)
-{
-    using var process = new Process
-    {
-        StartInfo = new ProcessStartInfo
-        {
-            FileName = "git",
-            Arguments = "branch",
-            WorkingDirectory = repoPath,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-        },
-    };
-
-    var response = $"{repoPath}> git pull";
-
-    var branhResponse = await RunProcessAsync(process, "branch --show-current");
-    if (branhResponse.IsSuccess)
-        response += $" {branhResponse.Response}";
-
-    var pullResponse = await RunProcessAsync(process, "pull");
-
-    response += pullResponse.Response;
-
-    return new PullResponse(pullResponse.IsSuccess, repoPath, response);
-}
-
-static async Task<ProcessResponse> RunProcessAsync(Process process, string args)
-{
-    process.StartInfo.Arguments = args;
-    process.Start();
-
-    var response = await process.StandardOutput.ReadToEndAsync();
-    var errorResponse = await process.StandardError.ReadToEndAsync();
-    var isSuccess = string.IsNullOrEmpty(errorResponse);
-
-    return new ProcessResponse(isSuccess,  response + errorResponse);
-}
-
-internal record ProcessResponse(bool IsSuccess, string Response);
-
-internal record PullResponse(bool IsSuccess, string Repo, string Response);
+// Console.ReadLine();
+//
+// if (args.Any(a => a == "--debug"))
+// {
+//     Console.WriteLine("Attaching debugger");
+//     Debugger.Launch();
+// }
+//
+// var currentDir = Directory.GetCurrentDirectory();
+// Console.WriteLine("Searchinng repos in " + currentDir);
+//
+// var repos = Directory.GetDirectories("./", "*", SearchOption.TopDirectoryOnly)
+//     .Where(r => Directory.GetDirectories(r, ".git", SearchOption.TopDirectoryOnly).Any())
+//     .ToArray();
+// if (!repos.Any())
+// {
+//     Console.WriteLine("No repositories found");
+//     return;
+// }
+//
+// Console.WriteLine("Detected repos:" + Environment.NewLine + string.Join(Environment.NewLine, repos));
+// Console.WriteLine();
+// Console.WriteLine("---PULLING STARTED---");
+// Console.WriteLine();
+//
+// var processes = repos.Select(r => PullWithHandling(r)).ToList();
+// var responses = new List<PullResponse>();
+// while (processes.Any())
+// {
+//     var finished = await Task.WhenAny(processes);
+//     processes.Remove(finished);
+//     var response = await finished;
+//     responses.Add(response);
+//
+//     Console.WriteLine(response.Response);
+//     Console.WriteLine("---");
+//     Console.WriteLine();
+// }
+//
+// Console.WriteLine("---PULLING FINISHED---");
+//
+// var havingError = responses.Where(r => !r.IsSuccess).Select(r => r.Repo).ToList();
+// var havingChanges = responses
+//     .Where(r => !r.Response.Contains("Already up to date.") &&
+//                 !havingError.Contains(r.Repo))
+//     .Select(r => r.Repo)
+//     .ToList();
+//
+// PrintAsList(havingChanges, "Changes applied");
+// PrintAsList(havingError, "Responded error");
+//
+// static void PrintAsList(List<string> items, string name)
+// {
+//     Console.WriteLine();
+//     Console.WriteLine($"---{name}---");
+//     Console.WriteLine(items.Any() ? string.Join(Environment.NewLine, items) : "no data");
+//     Console.WriteLine("---------------------");
+// }
+//
+// static async Task<PullResponse> PullWithHandling(string repoPath)
+// {
+//     try
+//     {
+//         return await Pull(repoPath);
+//     }
+//     catch (Exception e)
+//     {
+//         return new PullResponse(false, repoPath, $"Error pulling '{repoPath}'" + e.Message);
+//     }
+// }
+//
+// static async Task<PullResponse> Pull(string repoPath)
+// {
+//     using var process = new Process
+//     {
+//         StartInfo = new ProcessStartInfo
+//         {
+//             FileName = "git",
+//             Arguments = "branch",
+//             WorkingDirectory = repoPath,
+//             UseShellExecute = false,
+//             CreateNoWindow = true,
+//             RedirectStandardOutput = true,
+//             RedirectStandardError = true,
+//         },
+//     };
+//
+//     var response = $"{repoPath}> git pull";
+//
+//     var branhResponse = await RunProcessAsync(process, "branch --show-current");
+//     if (branhResponse.IsSuccess)
+//         response += $" {branhResponse.Response}";
+//
+//     var pullResponse = await RunProcessAsync(process, "pull");
+//
+//     response += pullResponse.Response;
+//
+//     return new PullResponse(pullResponse.IsSuccess, repoPath, response);
+// }
+//
+// static async Task<ProcessResponse> RunProcessAsync(Process process, string args)
+// {
+//     process.StartInfo.Arguments = args;
+//     process.Start();
+//
+//     var response = await process.StandardOutput.ReadToEndAsync();
+//     var errorResponse = await process.StandardError.ReadToEndAsync();
+//     var isSuccess = string.IsNullOrEmpty(errorResponse);
+//
+//     return new ProcessResponse(isSuccess,  response + errorResponse);
+// }
+//
+// internal record ProcessResponse(bool IsSuccess, string Response);
+//
+// internal record PullResponse(bool IsSuccess, string Repo, string Response);
